5099525ccb02abe4aec5b041c59826d570280dae
Mike Zaleski <me@mikezaleski.com>
2022-01-18 14:37:31 -0500

    dynamic backup button, video list from js object - pull in video data from js object - change backup button to show Backup or Primary based on state - Added native controls to player

---
+++ src/VideoData.js
@@ -0,0 +1,30 @@
+const videoData = [
+  {
+    id: "4bf88144-b49e-427d-9151-19eaf203d077",
+    title: "Clifflix: Illuminations Reflections of Earth 2021",
+    primary: "https://vimeo.com/566374660#t=3m43s",
+    backup: "https://www.youtube.com/watch?v=_wL7Y_9iLyI?t=223",
+    startTime: 223,
+  },
+  {
+    id: "a4102a1c-fba5-452f-b696-3381c7668970",
+    title: "Clifflix: Happily Ever After in 8k",
+    primary: "https://youtu.be/Qzvg9KuvRiw?t=109",
+    startTime: 109,
+  },
+  {
+    id: "0022b17e-1ce0-4ea4-ba48-de48fc7ff507",
+    title: "Clifflix: World of Color 2020",
+    primary: "https://vimeo.com/417445463#t=2m03s",
+    backup: "https://youtu.be/g4GmKYrzM0w?t=123",
+    startTime: 123,
+  },
+  {
+    id: "0308979a-0c6c-49c8-a588-165b9c8f3f86",
+    title: "Reflections of Earth with Peace on Earth Finale 2016",
+    primary: "https://youtu.be/AGYne2pBWTY?t=784",
+    startTime: 784,
+  },
+];
+
+export default videoData;
--- src/components/VideoListCard.js
+++ src/components/VideoListCard.js
@@ -1,4 +1,5 @@
 import { Card, Stack } from "react-bootstrap";
+import videoData from "../VideoData";
 
 export default function VideoListCard({ handleOpenVideo }) {
   return (
@@ -8,51 +9,23 @@ export default function VideoListCard({ handleOpenVideo }) {
           <div>List of Videos</div>
         </Card.Title>
         <Stack direction="vertical" gap="2">
-          <div
-            style={{ cursor: "pointer" }}
-            onClick={() =>
-              handleOpenVideo({
-                primary: "https://vimeo.com/566374660#t=3m43s",
-                backup: "https://www.youtube.com/watch?v=_wL7Y_9iLyI?t=223",
-              })
-            }
-          >
-            Clifflix: Illuminations Reflections of Earth 2021
-          </div>
-          <div
-            style={{ cursor: "pointer" }}
-            onClick={() =>
-              handleOpenVideo({
-                primary: "https://youtu.be/Qzvg9KuvRiw?t=109",
-              })
-            }
-          >
-            Clifflix: Happily Ever After in 8k
-          </div>
-          <div
-            style={{ cursor: "pointer" }}
-            onClick={() =>
-              handleOpenVideo({
-                primary: "https://vimeo.com/417445463#t=2m03s",
-                backup: "https://youtu.be/g4GmKYrzM0w?t=123",
-              })
-            }
-          >
-            Clifflix: World of Color 2020
-          </div>
-          <div
-            style={{ cursor: "pointer" }}
-            onClick={() =>
-              handleOpenVideo({
-                primary: "https://youtu.be/AGYne2pBWTY?t=784",
-              })
-            }
-          >
-            Reflections of Earth with Peace on Earth Finale 2016
-          </div>
-          <div>Video 3</div>
-          <div>Video 4</div>
-          <div>Video 5</div>
+          <ul>
+            {videoData.map((video) => (
+              <li
+                key={video.title}
+                style={{ cursor: "pointer" }}
+                onClick={() =>
+                  handleOpenVideo({
+                    primary: video.primary,
+                    backup: video?.backup,
+                    startTime: video.startTime,
+                  })
+                }
+              >
+                {video.title}
+              </li>
+            ))}
+          </ul>
         </Stack>
       </Card.Body>
     </Card>
