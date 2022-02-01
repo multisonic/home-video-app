841f7a483c120724a26cfc49c519ca719c07db2e
Mike Zaleski <me@mikezaleski.com>
2022-02-01 11:38:40 -0500

    refactor video url to use a slug also add basic video title and description boilerplate

--- src/pages/VideoPage.js
+++ src/pages/VideoPage.js
@@ -1,15 +1,30 @@
+import { Button, Card, Stack, Table } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import VideoPlayer from "../components/VideoPlayer";
-import { getVideo } from "../VideoData";
+import { getVideoBySlug } from "../VideoData";
 
 export default function VideoPage() {
   let params = useParams();
-  let video = getVideo(params.videoId);
+  let video = getVideoBySlug(params.slug);
   return (
     <>
-      <h1>Video Page</h1>
       <VideoPlayer video={video} />
-      <h2>{video.title}</h2>
+      <h1>{video.title}</h1>
+      <Stack direction="horizontal" gap="5">
+        <div>
+          <h2>Description</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Reprehenderit optio repellendus deserunt inventore, ratione
+            temporibus voluptatem ullam repellat. Id, libero animi?
+            Perspiciatis, nulla numquam. Aspernatur est alias esse iusto
+            doloribus! Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Possimus, libero natus quae minima quidem veritatis impedit ullam
+            nobis fugit reprehenderit. Corporis, ipsum? Pariatur debitis id
+            repellat nisi expedita beatae qui.
+          </p>
+        </div>
+      </Stack>
     </>
   );
 }
