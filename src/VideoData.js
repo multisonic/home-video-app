08b8acba53c8b333d88fdf40eabc7011d84df3ef
Mike Zaleski <me@mikezaleski.com>
2022-02-01 12:26:04 -0500

    work on layout of video page with react-bootstrap grid

--- src/VideoData.js
+++ src/VideoData.js
@@ -6,6 +6,8 @@ let videos = [
     backup: "https://www.youtube.com/watch?v=_wL7Y_9iLyI",
     startTime: 223,
     slug: "clifflix-roe-2021",
+    description:
+      'This is my latest 2021 version of "Reflections of Earth". This is a 4k Multicam edit with a multichannel studio soundtrack and is a HUGE upgrade to my previous three older versions. \n\n YouTube compression is brutal on these shadows! (even in 4k)  So, you can download your own high-quality copy of this video now down below.As always, if you like what you see, give it a "thumbs up" and subscribe to the channel. This video was made for all RoE fans around the world so make sure to tell all your friends about it and spread the word. Here is a link for a high-quality video file for your collection. In many ways, this is a much higher quality video than what YouTube can show online.',
   },
   {
     id: "a4102a1c-fba5-452f-b696-3381c7668970",
@@ -21,6 +23,8 @@ let videos = [
     backup: "https://youtu.be/g4GmKYrzM0w",
     startTime: 123,
     slug: "clifflix-woc-2020",
+    description:
+      "This is my \"World Of Color\" 2020 4k multi-cam edit. This is (almost) the original version of the show. It was filmed in 2019 during D23 Expo. It's not perfect but I think it's pretty good considering the footage I was able to bring home.  Let me know what you think! \n\n YouTube compression is brutally SAVAGE and really clamps down on shadows and night sky! I have created a nice, high quality video file for anybody to download and watch from their computer. In many ways, it's better than YouTube's heavily compressed 4k stream.",
   },
   {
     id: "0308979a-0c6c-49c8-a588-165b9c8f3f86",
@@ -28,6 +32,14 @@ let videos = [
     primary: "https://youtu.be/AGYne2pBWTY",
     startTime: 784,
     slug: "roe-christmas-2016",
+  },
+  {
+    id: "b97bcd59-836f-4542-8291-aa59887a9cf1",
+    title: "Eddie Films: Fantasmic 2020",
+    primary: "https://www.youtube.com/watch?v=gkU6vL4oAFE",
+    startTime: 0,
+    slug: "eddie-fantasmic-2020",
+    description: "",
   },
 ];
 
