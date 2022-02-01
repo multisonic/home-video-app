ed41bda83e78378ac3227cda27b244a7120e1d3b
Mike Zaleski <me@mikezaleski.com>
2022-02-01 12:41:22 -0500

    Add Header component with bootstrap Navbar

--- src/App.js
+++ src/App.js
@@ -1,40 +1,36 @@
 import { Button, Container, Stack } from "react-bootstrap";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
 import HomePage from "./pages/HomePage";
 import VideoPage from "./pages/VideoPage";
 
 function App() {
   return (
-    <Container className="my-2 d-flex flex-column">
-      <Stack direction="horizontal" gap="2" className="mb-3">
-        <div className="fs-5 me-auto">
-          <a href="/" className="text-decoration-none text-dark">
-            Home Video App
-          </a>
-        </div>
-        <Button>Test</Button>
-      </Stack>
-      <Router>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/videos/" element={<VideoPage />}>
-            <Route path=":slug" element={<VideoPage />} />
-          </Route>
-          <Route
-            path="*"
-            element={
-              <main style={{ padding: "1rem" }}>
-                <p>There's nothing here!</p>
-              </main>
-            }
-          />
-        </Routes>
-      </Router>
-      {/* {urlObject && (
+    <>
+      <Header />
+      <Container className="my-2 d-flex flex-column">
+        <Router>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/videos/" element={<VideoPage />}>
+              <Route path=":slug" element={<VideoPage />} />
+            </Route>
+            <Route
+              path="*"
+              element={
+                <main style={{ padding: "1rem" }}>
+                  <p>There's nothing here!</p>
+                </main>
+              }
+            />
+          </Routes>
+        </Router>
+        {/* {urlObject && (
         <VideoPlayer urlObject={urlObject} setUrlObject={setUrlObject} />
       )}
       <VideoListCard handleOpenVideo={handleOpenVideo} /> */}
-    </Container>
+      </Container>
+    </>
   );
 }
 
