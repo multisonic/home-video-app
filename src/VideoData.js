21b9f72fd9c068ebe927356f9adbf8e2951882d3
Mike Zaleski <me@mikezaleski.com>
2022-02-07 22:53:55 -0500

    added home video and type to VideoData.js

--- src/VideoData.js
+++ src/VideoData.js
@@ -1,6 +1,7 @@
 let videos = [
   {
     id: "4bf88144-b49e-427d-9151-19eaf203d077",
+    type: "disney",
     title: "Clifflix: Illuminations Reflections of Earth 2021",
     eventDate: "2021-09-01",
     postDate: "2021-09-30",
@@ -45,6 +46,7 @@ let videos = [
   },
   {
     id: "a4102a1c-fba5-452f-b696-3381c7668970",
+    type: "disney",
     title: "Clifflix: Happily Ever After in 8k",
     eventDate: "2021-09-01",
     postDate: "2021-09-30",
@@ -66,6 +68,7 @@ let videos = [
   },
   {
     id: "0022b17e-1ce0-4ea4-ba48-de48fc7ff507",
+    type: "disney",
     title: "Clifflix: World of Color 2020",
     primary: "https://vimeo.com/417445463",
     backup: "https://youtu.be/g4GmKYrzM0w",
@@ -75,7 +78,32 @@ let videos = [
       "This is my \"World Of Color\" 2020 4k multi-cam edit. This is (almost) the original version of the show. It was filmed in 2019 during D23 Expo. It's not perfect but I think it's pretty good considering the footage I was able to bring home.  Let me know what you think! \n\n YouTube compression is brutally SAVAGE and really clamps down on shadows and night sky! I have created a nice, high quality video file for anybody to download and watch from their computer. In many ways, it's better than YouTube's heavily compressed 4k stream.",
   },
   {
+    id: "3ec3aba8-700b-47a5-ae95-ad92b6bf5bb5",
+    type: "disney",
+    title: "Clifflix: Enchantment - Filmed in 8k",
+    eventDate: "2021-12-15",
+    postDate: "2021-12-30",
+    primary: "https://youtu.be/tGaoJ3WszWQ",
+    startTime: 96,
+    slug: "clifflix-enchantment",
+    description:
+      'This is my new 2021 Happily Ever After. It was produced in 8k resolution and uses my new recording technique called "Deep Space" audio! Let me know what you think of it and make SURE to download your own super high-quality video file!',
+    chapters: [
+      {
+        num: 1,
+        time: 0,
+        title: "Introduction",
+      },
+      {
+        num: 2,
+        time: 96,
+        title: "Enchantment",
+      },
+    ],
+  },
+  {
     id: "0308979a-0c6c-49c8-a588-165b9c8f3f86",
+    type: "disney",
     title: "Reflections of Earth with Peace on Earth Finale 2016",
     eventDate: "2021-09-01",
     postDate: "2021-09-30",
@@ -102,6 +130,7 @@ let videos = [
   },
   {
     id: "b97bcd59-836f-4542-8291-aa59887a9cf1",
+    type: "disney",
     title: "Eddie Films: Fantasmic 2020",
     eventDate: "2021-09-01",
     postDate: "2021-09-30",
@@ -110,10 +139,72 @@ let videos = [
     slug: "eddie-fantasmic-2020",
     description: null,
   },
+  {
+    id: "156a262f-fe68-4a33-b3f1-15df7a2abfc0",
+    type: "home-video",
+    title: "1990 Walt Disney Vacation",
+    eventDate: "1990-01-01",
+    postDate: "1990-01-01",
+    primary: "https://vimeo.com/126764991/8f9a34e4ee",
+    startTime: 0,
+    slug: "wdw-1990",
+    description: "Tape 113 (JZ 13)",
+    chapters: [
+      {
+        num: 1,
+        time: 0,
+        title: "Superstar Television",
+      },
+      {
+        num: 2,
+        time: 19,
+        title: "'Hollywood, Hollywood' Show",
+      },
+      {
+        num: 3,
+        time: 329,
+        title: "Citizens of Hollywood",
+      },
+      {
+        num: 4,
+        time: 354,
+        title: "Prime Time Cafe",
+      },
+      {
+        num: 5,
+        time: 600,
+        title: "Screen Xtras Saxophone Quintet",
+      },
+      {
+        num: 6,
+        time: 672,
+        title: "Michael w/ Screen Xtras",
+      },
+      {
+        num: 7,
+        time: 752,
+        title: "Swan & Dolphin + Construction",
+      },
+      {
+        num: 8,
+        time: 937,
+        title: "Pleasure Island",
+      },
+      {
+        num: 9,
+        time: 1097,
+        title: "Comedy Warehouse",
+      },
+    ],
+  },
 ];
 
 export function getVideos() {
   return videos;
+}
+
+export function getVideosByType(type) {
+  return videos.filter((video) => video.type === type);
 }
 
 export function getVideoById(id) {
